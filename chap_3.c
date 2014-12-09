235e79d12c321c77a9e0db746a63c158370a6ce0
Mike Bukowski <michael.r.bukowski@gmail.com>
2014-12-08 19:49:34 -0500

    First commit

---
+++ chap_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int difference;
+
+    a = 87;
+    b = 15;
+    difference = a - b;
+
+    printf("%i subtracted from %i is %i \n", a, b, difference );
+}
